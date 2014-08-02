50a5ae8fcc862ad47dbff31fc374da615affcbd3
azurecurve <azurecurve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-02 12:57:23 +0000

    1.02 Fixed security issue
    git-svn-id: https://plugins.svn.wordpress.org/azurecurve-floating-featured-image/tags/1.0.0@959217 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- azurecurve-floating-featured-image.php
+++ azurecurve-floating-featured-image.php
@@ -27,7 +27,7 @@ The full copy of the GNU General Public License is available here: http://www.gn
 
 register_activation_hook( __FILE__, 'azc_ffi_set_default_options' );
 
-function azc_ffi_set_default_options() {
+function azc_ffi_set_default_options($networkwide) {
 	
 	$new_options = array(
 				'default_path' => plugin_dir_url(__FILE__).'images/',
@@ -38,31 +38,35 @@ function azc_ffi_set_default_options() {
 				'default_taxonomy_is_tag' => 0
 			);
 	
-	// set defaults for single site
-	if ( ! is_multisite() ) {
+	// set defaults for multi-site
+	if (function_exists('is_multisite') && is_multisite()) {
+		// check if it is a network activation - if so, run the activation function for each blog id
+		if ($networkwide) {
+			global $wpdb;
 
-		if ( get_option( 'azc_ffi_options' ) === false ) {
-			add_option( 'azc_ffi_options', $new_options );
-		}
-	}
-	//set defaults for multi-site
-	else{
-		global $wpdb;
+			$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+			$original_blog_id = get_current_blog_id();
 
-		$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-		$original_blog_id = get_current_blog_id();
+			foreach ( $blog_ids as $blog_id ) {
+				switch_to_blog( $blog_id );
 
-		foreach ( $blog_ids as $blog_id ) {
-			switch_to_blog( $blog_id );
-
-			foreach ( $options as $option ) {
 				if ( get_option( 'azc_ffi_options' ) === false ) {
 					add_option( 'azc_ffi_options', $new_options );
 				}
 			}
-		}
 
-		switch_to_blog( $original_blog_id );
+			switch_to_blog( $original_blog_id );
+		}else{
+			if ( get_option( 'azc_ffi_options' ) === false ) {
+				add_option( 'azc_ffi_options', $new_options );
+			}
+		}
+	}
+	//set defaults for single site
+	else{
+		if ( get_option( 'azc_ffi_options' ) === false ) {
+			add_option( 'azc_ffi_options', $new_options );
+		}
 	}
 }
 
